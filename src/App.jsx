393e1b30967a554590558d42160f02f25c9813ba
cookie <kwiek1215@gmail.com>
2022-07-11 23:26:02 -0300

    desafio 2

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import navbar from './Componentes/navbar';
+
+function App() {
+  return (
+    <navbar />
+  );
+}
+
+export default App;
---
+++ src/Componentes/navbar/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const NAVAR = () => {
+    return (
+        <div class="navar">
+            <nav>
+                <div class="titulo">
+                    <a>SHIUMI</a>
+                </div>
+                <div class="opciones">
+                    <ul>
+                        <li>Sobre nosotros</li>
+                        <li>Contacto</li>
+                        <li>Noticias</li>
+                        <li>Tienda</li>
+                    </ul>
+                </div>
+            </nav> 
+
+        </div>
+    );
+}
+
+export default NAVAR; 
